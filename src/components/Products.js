12bb00be09bd2e4bd6f6933fd99622d5ec04d705
‘Ibra‘ <ibracortes@gmail.com>
2022-09-19 11:54:10 +0200

    get products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,25 +1,53 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Product from './Product';
 import { Typography } from '@mui/material';
 
 const Products = () => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const { data: response } = await axios.get("http://localhost:8000/product/get");
+        setData(response);
+      } catch (error) {
+        console.error(error.message);
+      }
+      setLoading(false);
+    }
+
+    fetchData();
+  }, []);
+
+  console.log("DATA: ", data)
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Typography variant="h4" color="ActiveBorder" padding={5} textAlign="center">
         Nuestros Vinos
       </Typography>
-      <Grid container spacing={{ xs: 6, md: 3, sm: 2 }} columns={{ xs: 6, sm: 12, md: 12, lg: 16 }}>
-        {Array.from(Array(10)).map((_, index) => (
-          <Grid key={index} item xs={6} sm={6} md={4} lg={3} >
-            <Product />
-          </Grid>
-        ))}
-      </Grid>
 
+      {loading && <div>Loading</div>}
+
+      {!loading && (
+        <Grid container spacing={{ xs: 6, md: 3, sm: 2 }} columns={{ xs: 6, sm: 12, md: 12, lg: 16 }}>
+          {data.map((item, index) => {
+            console.log("ITEM: ", item)
+            return (
+              <Grid key={index} item xs={6} sm={6} md={4} lg={3} >
+                <Product />
+              </Grid>
+            )
+          }
+          )}
+        </Grid>
+      )}
     </Box>
-
   )
 }
 
